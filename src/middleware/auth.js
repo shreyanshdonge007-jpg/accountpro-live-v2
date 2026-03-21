db3a69765e3159977631c51fa2d7f4d6de6ff71a
Your Name <your.email@example.com>
2026-03-22 05:15:09 +0530

    Fix jwt path in auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,4 @@
-const { verifyToken } = require('./jwt');
+const { verifyToken } = require('..config/jwt');
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
